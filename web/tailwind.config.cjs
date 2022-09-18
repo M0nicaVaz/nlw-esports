433b557d25c2b948f4ef79291c64a1a8c8575d9a
Monica <contatomonicavaz@gmail.com>
2022-09-18 11:18:33 -0300

    chore: web config

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.tsx'],
+
   theme: {
     fontFamily: {
       sans: ['Inter', 'sans-serif'],
@@ -15,5 +16,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-radix')()],
 };
